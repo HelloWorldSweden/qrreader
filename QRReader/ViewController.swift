fb28e42c7d9ae566391651493b980fc3ad78a810
davidwolters <david@wolters.se>
2017-05-25 13:40:00 +0200

    Initial Commit

---
+++ QRReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRReader
+//
+//  Created by David Wolters on 5/25/17.
+//  Copyright © 2017 David Wolters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
